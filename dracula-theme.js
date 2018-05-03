9d503ee7cc66741b4a2749f232c0282dbe00ab50
Lewis Steele <lewis.steele@me.com>
2018-05-03 19:03:46 +0000

    Added theme file

---
+++ dracula-theme.js
@@ -0,0 +1,19 @@
+term_.prefs_.set("background-color", "#282a36");
+term_.prefs_.set("foreground-color", "#f8f8f2");
+term_.prefs_.set("cursor-color", "#f8f8f2");
+term_.prefs_.set("enable-bold", true);
+
+const base0 = "";
+
+const cyan = "#8be9fd";
+const green = "#50fa7b";
+const orange = "#ffb86c";
+const pink = "#ff79c6";
+const purple = "#bd93f9";
+const red = "#ff5555";
+const yellow = "#f1fa8c";
+
+term_.prefs_.set("color-palette-overrides", [
+
+]);
+
