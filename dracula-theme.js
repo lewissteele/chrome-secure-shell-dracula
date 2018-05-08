3aab125dbf421fff1200c6b64b66196313ed964d
Lewis Steele <lewis.steele@me.com>
2018-05-08 21:16:35 +0000

    Added colours for theme

--- dracula-theme.js
+++ dracula-theme.js
@@ -3,17 +3,45 @@ term_.prefs_.set("foreground-color", "#f8f8f2");
 term_.prefs_.set("cursor-color", "#f8f8f2");
 term_.prefs_.set("enable-bold", true);
 
-const base0 = "";
+// Background Tones
+const base03 = "#282a36";
+const base02 = "#282a36";
 
+// Content Tones
+const base01 = "#44475a";
+const base00 = "#44475a";
+const base0 = "#44475a";
+const base1 = "#44475a";
+
+// Background Tones
+const base2 = "#44475a";
+const base3 = "#44475a";
+
+// Accent Tones
+const yellow = "#f1fa8c";
+const orange = "#ffb86c";
+const red = "#ff5555";
+const magenta = "#ff79c6";
+const violet = "#bd93f9";
+const blue = "#8be9fd";
 const cyan = "#8be9fd";
 const green = "#50fa7b";
-const orange = "#ffb86c";
-const pink = "#ff79c6";
-const purple = "#bd93f9";
-const red = "#ff5555";
-const yellow = "#f1fa8c";
 
 term_.prefs_.set("color-palette-overrides", [
-
+    base03,
+    base02,
+    base01,
+    base00,
+    base0,
+    base1,
+    base2,
+    base3,
+    yellow,
+    orange,
+    red,
+    magenta,
+    blue,
+    cyan,
+    green
 ]);
 
